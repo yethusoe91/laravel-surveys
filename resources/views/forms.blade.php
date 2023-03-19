b2543735453732ce998864cd0dd33b2d434cfb6b
Ye Thu Soe <mr.yethusoe@gmail.com>
2023-03-19 13:28:54 +0630

    minor update for ux

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -16,11 +16,15 @@
     </style>
 </head>
 <body class="antialiased">
-    <div class="relative min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    <div class="relative min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:texst-white">
 
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="mt-16">
                 <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-8">
+                    
+                    @if(!$forms->count())
+                        <h4 class="text-center text-gray-900 dark:text-white">There is no surveys form at the moment!</h4>
+                    @endif
                     @foreach ($forms as $form)
                     <a href="{{ route('show.form', $form->id)}}" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                         <div>
